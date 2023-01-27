a24ea0e471fb247a20e30f4f3356fbb613010ef5
Harvey Fu <harveyfu@usc.edu>
2023-01-27 05:34:46 +0000

    hw1 coding part1

--- split.cpp
+++ split.cpp
@@ -13,11 +13,41 @@ the function below should be the only one in this file.
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
+void push_back(Node*& in, Node*& add);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+  // WRITE YOUR CODE HERE
+  if (in==nullptr){
+    return;
+  }
+  else if ((in->value)%2==0){
+    Node* tmp=in;
+    in=in->next;
+    tmp->next=nullptr;
+    push_back(evens, tmp);
+    split(in, odds, evens);
+  }
+  else if((in->value)%2==1){
+    Node* tmp=in;
+    in=in->next;
+    tmp->next=nullptr;
+    push_back(odds, tmp);
+    split(in, odds, evens);
+  }
 }
 
 /* If you needed a helper function, write it here */
+void push_back(Node*& in, Node*& add){
+  // push a Node pointer to the end of the list recursively, need to preprocess add such that add->next = nullptr
+  if (in == nullptr){
+    in=add;
+  }
+  else if(in->next == nullptr){
+    in->next = add;
+  }
+  else{
+    push_back(in->next, add);
+  }
+}
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,53 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+  Node d(0, nullptr);
+  Node* odds = nullptr;
+  Node* evens = nullptr;
+  Node f8(14, nullptr);
+  Node f7(11, &f8);
+  Node f6(8, &f7);
+  Node f5(5, &f6);
+  Node f4(4, &f5);
+  Node f3(3, &f4);
+  Node f2(2, &f3);
+  Node f1(1, &f2);
+
+  Node* tmp=&f1;
+
+  while(tmp!=nullptr){
+    cout << tmp->value << endl;
+    tmp=tmp->next;
+  }
+  Node* test=&f1;
+
+  split(test, odds, evens);
+  if (test==nullptr){
+    cout<<"success"<< endl;
+  }
+  else{
+    cout<<"fail"<<endl;
+  }
+  
+  Node* tmp_odds=odds;
+
+  while(tmp_odds!=nullptr){
+    cout << tmp_odds->value << " ";
+    tmp_odds=tmp_odds->next;
+  }
+  cout << endl;
+  Node* tmp_evens=evens;
+
+  while(tmp_evens!=nullptr){
+    cout << tmp_evens->value << " ";
+    tmp_evens=tmp_evens->next;
+  }
+
 
 }
